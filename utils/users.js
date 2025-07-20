465d11fa4bf750249726751cbdf1d8bbad77b50a
Abhinav Awasthi <56489028+abhinavawasthi1729@users.noreply.github.com>
2025-07-20 23:00:30 +0530

    Update users.js

--- utils/users.js
+++ utils/users.js
@@ -1,33 +1,32 @@
+// Store connected users
 const users = [];
 
-// Join user to chat
+// Add a new user to the chat
 function userJoin(id, username, room) {
   const user = { id, username, room };
-
   users.push(user);
-
   return user;
 }
 
-// Get current user
+// Retrieve a user by their socket ID
 function getCurrentUser(id) {
   return users.find(user => user.id === id);
 }
 
-// User leaves chat
+// Remove a user when they leave the chat
 function userLeave(id) {
   const index = users.findIndex(user => user.id === id);
-
   if (index !== -1) {
-    return users.splice(index, 1)[0];
+    return users.splice(index, 1)[0]; // Return removed user
   }
 }
 
-// Get room users
+// Get all users in a specific chat room
 function getRoomUsers(room) {
   return users.filter(user => user.room === room);
 }
 
+// Export functions for use in other files
 module.exports = {
   userJoin,
   getCurrentUser,
